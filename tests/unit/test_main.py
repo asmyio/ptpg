9ca982981111352c5286ecd8070bceeae105ee52
Ahmad Siraj MY <28752140+asmyio@users.noreply.github.com>
2022-10-07 09:50:25 +0700

    remove: test_main

--- tests/unit/test_main.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from main import hello_world
-
-@pytest.mark.parametrize("expected", [
-    'ini semua dunia'
-])
-def test_response(expected):
-    # returns string
-    assert hello_world() == expected

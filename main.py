79b2121145be587c3e987d6bbdf67b828f1628dc
Ahmad Siraj MY <28752140+asmyio@users.noreply.github.com>
2022-10-07 09:45:35 +0700

    feat: added event driven function

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
+# This is an event driven function
 def hello_world():
     return f'ini semua dunia'
 
-def main():
-    hello_world()
-
-if __name__ == '__main__':
-  main()
+def main(event, context):
+    print('Event ID: {}'.format(context.event_id))
+    print('Event type: {}'.format(context.event_type))
+    print('Bucket: {}'.format(event['bucket']))
+    print('File: {}'.format(event['name']))
+    print('Metageneration: {}'.format(event['metageneration']))
+    print('Created: {}'.format(event['timeCreated']))
+    print('Updated: {}'.format(event['updated']))
